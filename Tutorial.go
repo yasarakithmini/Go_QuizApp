402dfd854b2837616be8314f60263e2ca4eecabb
yasarakithmini <123257361+yasarakithmini@users.noreply.github.com>
2025-03-07 23:31:57 +0530

    commit 02

--- Tutorial.go
+++ Tutorial.go
@@ -4,4 +4,24 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to my quiz game!")
+
+	fmt.Printf("Enter your name: ")
+
+	var name string
+
+	fmt.Scan(&name)
+
+	fmt.Printf("Hello %v, Welcome to the game!\n", name)
+
+	fmt.Println ("Enter your age: ")
+	var age uint
+	fmt.Scan(&age)
+
+	if age >= 10 {
+		fmt.Println("Yay you can play!")
+	} else {
+	fmt.Println("You cannot play!")
+	return
+	}
+fmt.Printf("What is ")
 }
