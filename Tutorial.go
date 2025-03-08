63e57de605efcb84d676925f5ecfc0828cdeb54f
yasarakithmini <123257361+yasarakithmini@users.noreply.github.com>
2025-03-08 20:55:55 +0530

    final

--- Tutorial.go
+++ Tutorial.go
@@ -23,5 +23,36 @@ func main() {
 	fmt.Println("You cannot play!")
 	return
 	}
-fmt.Printf("What is ")
+
+var score int = 0
+var num_questions=2
+fmt.Printf("What is better, RTX 3090 or RTX 3080? :")
+var answer string
+var answer2 string
+fmt.Scan(&answer, &answer2)
+
+if answer + " " + answer2 == "RTX 3090" {
+	fmt.Println("Correct")
+	score ++
+} else if answer + " " + answer2 == "rtx 3090" {
+		fmt.Println("Correct")
+		score ++
+} else {
+	fmt.Println("Incorrect")
+}
+
+fmt.Printf("How many cores does the Ryzen 9 3900x have? ")
+var cores int
+
+fmt.Scan(&cores)
+
+if cores == 12 {
+	fmt.Println("Correct")
+	score ++
+} else {
+	fmt.Println("Incorrect")
+}
+fmt.Printf("You scored %v out of %v.", score, num_questions)
+percent := (float64(score) / float64(num_questions)) * 100
+fmt.Printf("You scored: %v%%. ", percent )
 }
